51d39da7042f445bd4bda74705d574423df5de34
Ubuntu <ubuntu@ip-10-0-0-7.localdomain>
2012-12-11 11:16:18 +0900

    ログインページに飛ばすソースをいれた

--- public_html/game/scouter/application/views/template.php
+++ public_html/game/scouter/application/views/template.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="ja" xmlns:og="http://ogp.me/ns#"
-xmlns="http://www.w3.org/1999/xhtml"
-xmlns:fb="http://www.facebook.com/2008/fbml"
-itemscope itemtype="http://schema.org/Article">
+<?
+#fbログインのcheck
+require_once(__DIR__ .'/../../../../../util/fb-check.php');
+?>
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>Scouter!</title>
